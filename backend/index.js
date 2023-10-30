eeb21dc99a4a7708d7b5c9a28546c858b2bbe954
Shatrughan Kumar <123942835+shatrukumar47@users.noreply.github.com>
2023-10-31 01:50:34 +0530

    sentiment-analysis

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.post("/summary", async (req, res) => {
       messages: [
         {
           role: "assistant",
-          content: `Please summarize the given paragraph/article and try to make it brief but don't missout keywords and the meaning of article/paragraph. **${paragraph}**`,
+          content: `Please provide a concise summary of the key points, insights, and important details from the following multi-type documents/articles (list the document/article titles or content descriptions here). Summarize the content, highlighting any common themes or significant differences, and make sure to capture the most crucial information. Additionally, please analyze the overarching message or main takeaways. **${paragraph}**`,
         },
       ],
       temperature: 1.5,
@@ -76,16 +76,47 @@ app.post("/translation", async (req, res) => {
       messages: [
         {
           role: "assistant",
-          content: `Translate the following text into "${language}":  **${message}**.`,
+          content: `Translate the following message into [${language}]: '[${message}]'`,
         },
       ],
-      temperature: 1.5,
-      max_tokens: 279,
+      temperature: 1,
+      max_tokens: 1011,
       top_p: 1,
-      frequency_penalty: 1.01,
-      presence_penalty: 1.03,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
-    // console.log(response.choices);
+    res.status(200).send({ msg: response.choices[0].message.content });
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+//sentiment analyses
+app.post("/sentiment-analysis", async (req, res) => {
+  const { userMsg, language } = req.body;
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a sentiment analysis model.",
+        },
+        {
+          role: "user",
+          content: `Please analyze the sentiment of the following text in [${language}] and provide the result with the keywords Positive, Negative or Neutral.`,
+        },
+        {
+          role: "assistant",
+          content: `[${userMsg}]`,
+        },
+      ],
+      temperature: 1,
+      max_tokens: 326,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
     res.status(200).send({ msg: response.choices[0].message.content });
   } catch (error) {
     res.status(400).send({ error: error });
@@ -95,4 +126,3 @@ app.post("/translation", async (req, res) => {
 app.listen(8080, () => {
   console.log("Server is live at Port 8080");
 });
-
